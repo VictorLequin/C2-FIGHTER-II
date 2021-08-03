1700a82297e237376986696127c4b2684edcf9d9
Victor Lequin <victor.leq98@gmail.com>
2021-08-04 00:10:36 +0200

    +bumb (non fonctionnel)

--- Godot Project/scenes/stages/Plateforme.gd
+++ Godot Project/scenes/stages/Plateforme.gd
@@ -3,6 +3,9 @@ extends Area2D
 func _collide(area):
 	if area.get_parent().has_method("_land"):
 		area.get_parent()._land(self)
+	if area.has_method("_bump"):
+		if not area.just_landed:
+			area._bump(self)
 
 func _stop_collision(area):
 	if area.get_parent().has_method("_fall"):
