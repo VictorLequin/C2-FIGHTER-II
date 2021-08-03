77f9c12399a31d80534b9a2a6313413f5ec75ce3
Victor Lequin <victor.leq98@gmail.com>
2021-08-03 23:26:13 +0200

    +mouvements de base

--- Godot Project/scenes/persos/Perso.gd
+++ Godot Project/scenes/persos/Perso.gd
@@ -4,36 +4,63 @@ var screen_size
 var on_ground
 var sprite
 var velocity
-export var speed = 50
+export var walk_speed = 100
+export var air_acc = 200
+export var ground_frott_quad = 0.3
+export var ground_frott_stat = 1
 var direction
+var last_pos
+
+func _land(plateforme):
+	var y0 = plateforme.position.y - plateforme.get_node("CollisionShape2D").shape.extents.y
+	if last_pos.y < y0 and not on_ground:
+		on_ground = true
+		velocity.y = 0
+		position.y = y0
+
+func _fall():
+	on_ground = false
 
 func _ready():
 	screen_size = get_viewport_rect().size
-	on_ground = true
+	on_ground = false
 	sprite = $AnimatedSprite
-	sprite.play("idle")
 	velocity = Vector2()
 	direction = 1
+	last_pos = position
 
 func _physics_process(delta):
+	last_pos = position
 	var force = Vector2()
 	var vel_walk = Vector2()
 	if not on_ground:
 		force.y += gravity
-	velocity += force*delta
 	var direction_new = 0
 	if Input.is_action_pressed("ui_right"):
 		direction_new = 1
-		vel_walk.x += speed
+		vel_walk.x += walk_speed
+		if not on_ground:
+			force.x += air_acc
 	if Input.is_action_pressed("ui_left"):
-		vel_walk.x -= speed
+		vel_walk.x -= walk_speed
 		direction_new = -1
+		if not on_ground:
+			force.x -= air_acc
+	if on_ground:
+		force.x -= ground_frott_quad*velocity.x*abs(velocity.x)
+	velocity += force*delta
 	if direction * direction_new == -1:
 		direction = direction_new
 		scale.x = direction
-	if vel_walk.length() > 0:
-		sprite.play("walk")
-	elif on_ground:
-		sprite.play("idle")
+	if on_ground:
+		position += vel_walk*delta
+		if vel_walk.length() > 0:
+			sprite.play("walk")
+		else:
+			sprite.play("idle")
+	else:
+		sprite.play("air")
 	position += velocity*delta
-	position += vel_walk*delta
+	# TEMP
+	if position.y > screen_size.y:
+		position.y = 0
