b8d866a99014bcca2bc9e03048f0e9eefda26bd1
ShadokDuBas <vincent.peth@outlook.fr>
2021-08-03 17:36:21 +0200

    Mise à jour du menu de sélection des stages

--- Godot Project/scenes/menus/Quitter.gd
+++
@@ -1,7 +0,0 @@
-extends Label
-
-func _input(event):
-	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
-		var bounds = Rect2(rect_position, rect_size)
-		if bounds.has_point(event.position):
-			get_tree().quit()
