25f02d04efb25c5489dd98cac8c9581d8cde91d1
Swire42 <v_m_1732@orange.fr>
2021-08-04 15:42:15 +0200

    renamed most files for language constistency

--- Godot Project/scenes/menus/Choix stage.gd
+++
@@ -1,8 +0,0 @@
-extends Label
-
-
-func _input(event):
-	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
-		var bounds = Rect2(rect_position, rect_size)
-		if bounds.has_point(event.position):
-			get_node("/root/Node").load_menu("res://scenes/menus/Selection stage.tscn")
--- Godot Project/scenes/menus/Ecran titre.gd
+++
@@ -1,5 +0,0 @@
-extends Control
-
-func _unhandled_input(event):
-	if event.is_action("ui_accept"):
-		get_node("/root/Node").load_menu("res://scenes/menus/MenuPr.tscn")
