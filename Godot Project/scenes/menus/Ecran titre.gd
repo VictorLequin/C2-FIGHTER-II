c985e769e453edc78ea1e0d94848403e4f6ed1d5
Swire42 <v_m_1732@orange.fr>
2021-08-04 02:54:08 +0200

    Fixed multi input bug

--- Godot Project/scenes/menus/Ecran titre.gd
+++ Godot Project/scenes/menus/Ecran titre.gd
@@ -4,6 +4,6 @@ func goto_scene(path):
 	Utils.change_scene(self, path)
 
 func _unhandled_input(event):
-	if Input.is_action_just_pressed("ui_accept"):
+	if event.is_action("ui_accept"):
 		#get_tree().change_scene("res://scenes/menus/MenuPr.tscn")
 		call_deferred("goto_scene", "res://scenes/menus/MenuPr.tscn")
