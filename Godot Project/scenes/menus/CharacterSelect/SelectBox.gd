6d25b4a8a679a3a69cb9eb9b15537e8793806686
Swire42 <v_m_1732@orange.fr>
2021-08-18 18:41:35 +0200

    Fixed joypad char selection

--- Godot Project/scenes/menus/CharacterSelect/SelectBox.gd
+++ Godot Project/scenes/menus/CharacterSelect/SelectBox.gd
@@ -4,17 +4,21 @@ var player
 var ui_left: String
 var ui_right: String
 
+onready var lastUpdate = OS.get_ticks_msec()
+const input_delay = 100 # ms
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	reload()
 
 func _input(event):
-	if event.is_action_pressed(ui_left):
+	if event.is_action_pressed(ui_left) and lastUpdate + input_delay < OS.get_ticks_msec():
 		player.prevCharacter()
+		lastUpdate = OS.get_ticks_msec()
 		reload()
-	if event.is_action_pressed(ui_right):
+	if event.is_action_pressed(ui_right) and lastUpdate + input_delay < OS.get_ticks_msec():
 		player.nextCharacter()
+		lastUpdate = OS.get_ticks_msec()
 		reload()
 
 func reload():
