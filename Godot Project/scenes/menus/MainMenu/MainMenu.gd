347f7d1a0d52eacee8146cb93b807ebf77a59642
Victor Lequin <victor.leq98@gmail.com>
2021-08-19 14:39:03 +0200

    *reworked main menu

--- Godot Project/scenes/menus/MainMenu/MainMenu.gd
+++ Godot Project/scenes/menus/MainMenu/MainMenu.gd
@@ -1,21 +1,24 @@
 extends Control
 
-onready var boutJouer = $VBoxContainer/HBoxContainer2/VBoxContainer/Jouer
-onready var boutParam = $VBoxContainer/HBoxContainer2/VBoxContainer/Parametres
-onready var boutQuit = $VBoxContainer/HBoxContainer2/VBoxContainer/Quitter
+var boutJouer
+var boutParam
+var boutQuit
 
 func _ready():
+	boutJouer = $Jouer
+	boutParam = $Quitter
+	boutQuit = $Quitter
 	boutJouer.grab_focus()
-
+	boutJouer.connect("pressed", self, "_on_Jouer_pressed")
+	boutParam.connect("pressed", self, "_on_Parametres_pressed")
+	boutQuit.connect("pressed", self, "_on_Quitter_pressed")
 
 func _on_Jouer_pressed():
 	#get_node("/root/Node").load_menu("res://scenes/menus/CharacterSelect/CharacterSelect.tscn")
 	get_node("/root/Node").load_character_select()
 
-
 func _on_Parametres_pressed():
 	print("Paramètres")
-
 
 func _on_Quitter_pressed():
 	get_tree().quit()
